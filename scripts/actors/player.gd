9922340e2a3166e2d982021e8f095ecdfe082f4c
Dr-doublesharp <melkayar32@gmail.com>
2026-05-04 19:45:47 +0300

    player sound
    adding player sounds

--- scripts/actors/player.gd
+++ scripts/actors/player.gd
@@ -9,16 +9,27 @@ var _speed: float = walk_speed #dont touch it, this is changes automaticlly
 var direction:Vector2
 
 @onready var animated_sprite2D = $AnimatedSprite2D
+@onready var sfx_walk = $Sounds/sfx_walk
+@onready var sfx_dead = $Sounds/sfx_death
+
+@export var max_health: int = 4
+var health: int
 
 var anim
 
 
 func _ready() -> void:
 	animated_sprite2D.play('idle')
+	health = max_health
 
 func _process(delta: float) -> void:
 
-	anim = 'walk' if direction.length()>0 else 'idle'
+	if health <= 0:
+		anim = 'die'
+		isdead()
+		return
+
+	anim ='walk' if direction.length()>0 else 'idle'
 
 	if animated_sprite2D.animation != anim:
 		animated_sprite2D.play(anim)
@@ -28,8 +39,29 @@ func _process(delta: float) -> void:
 
 	_speed = sprint_speed if Input.is_action_pressed('sprint') else walk_speed
 
+	if direction.length() >0:
+		if not sfx_walk.playing:
+			sfx_walk.play()
+			sfx_walk.volume_db = 10
+	else:
+		if sfx_walk.playing:
+			var tween = create_tween()
+			tween.tween_property(sfx_walk,"volume_db", -80,0.1)
+			tween.tween_callback(sfx_walk.stop)
+
 
 func _physics_process(delta: float) -> void:
+	if health <=0:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return	
+
 	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = direction.normalized()*_speed
 	move_and_slide()
+
+func isdead():
+	if animated_sprite2D.animation !=anim:
+		animated_sprite2D.play('die')
+		sfx_walk.stop()
+		sfx_dead.play()

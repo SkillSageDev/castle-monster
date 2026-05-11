2f14e2a8bf4f85f4bb2b165e3f73f616b30f9082
SkillSageDev <ardevinde@gmail.com>
2026-05-11 06:16:38 +0300

    fix: code indentation

--- scripts/actors/monster/chase.gd
+++ scripts/actors/monster/chase.gd
@@ -18,74 +18,74 @@ var player
 
 
 func enter():
-    is_active = true
-    player = get_tree().get_first_node_in_group("player")
-    set_random_target()
-    animation_player.play('walk')
+	is_active = true
+	player = get_tree().get_first_node_in_group("player")
+	set_random_target()
+	animation_player.play('walk')
 
 
 func exit():
-    is_active = false
-    monster_character_body_2d.velocity = Vector2.ZERO
+	is_active = false
+	monster_character_body_2d.velocity = Vector2.ZERO
 
 
 func physics_update(_delta: float):
-    if not player: return
+	if not player: return
 
-    if nav_mesh_agent_2d.is_navigation_finished():
-        if is_active == true:
-            monster_character_body_2d.velocity = Vector2.ZERO
-            chase_time.stop()
-            transitioned.emit(self,attack_state.name)
-            return
+	if nav_mesh_agent_2d.is_navigation_finished():
+		if is_active == true:
+			monster_character_body_2d.velocity = Vector2.ZERO
+			chase_time.stop()
+			transitioned.emit(self,attack_state.name)
+			return
 
-    var next_path_position = nav_mesh_agent_2d.get_next_path_position()
-    var direction = monster_character_body_2d.global_position.direction_to(next_path_position)
-    monster_character_body_2d.velocity = direction * speed
-    monster_character_body_2d.move_and_slide()
+	var next_path_position = nav_mesh_agent_2d.get_next_path_position()
+	var direction = monster_character_body_2d.global_position.direction_to(next_path_position)
+	monster_character_body_2d.velocity = direction * speed
+	monster_character_body_2d.move_and_slide()
 
 
 func update(_delta: float):
-    
-    if Global.monster_health ==0:
-        chase_time.stop()
-        transitioned.emit(self,death_state.name)
+	
+	if Global.monster_health ==0:
+		chase_time.stop()
+		transitioned.emit(self,death_state.name)
 
-    var dist_to_target = player.global_position.distance_to(nav_mesh_agent_2d.target_position)
-    if dist_to_target > max_distance:
-        set_random_target()
-    if not nav_mesh_agent_2d.is_navigation_finished():
-        if monster_character_body_2d.velocity.x <-0.1:
-            flip_container.scale.x = -1
-        elif monster_character_body_2d.velocity.x > 0.1:
-            flip_container.scale.x = 1
+	var dist_to_target = player.global_position.distance_to(nav_mesh_agent_2d.target_position)
+	if dist_to_target > max_distance:
+		set_random_target()
+	if not nav_mesh_agent_2d.is_navigation_finished():
+		if monster_character_body_2d.velocity.x <-0.1:
+			flip_container.scale.x = -1
+		elif monster_character_body_2d.velocity.x > 0.1:
+			flip_container.scale.x = 1
 
 
 func set_random_target():
-    if not player: return
-    var random_angle = randf() *TAU
-    var random_radius = randf_range(min_distance,max_distance)
-    var target_vector = player.global_position + Vector2.from_angle(random_angle) * random_radius
-    var map_rid = monster_character_body_2d.get_world_2d().navigation_map
-    var final_point = NavigationServer2D.map_get_closest_point(map_rid,target_vector)
-    nav_mesh_agent_2d.target_position = final_point
+	if not player: return
+	var random_angle = randf() *TAU
+	var random_radius = randf_range(min_distance,max_distance)
+	var target_vector = player.global_position + Vector2.from_angle(random_angle) * random_radius
+	var map_rid = monster_character_body_2d.get_world_2d().navigation_map
+	var final_point = NavigationServer2D.map_get_closest_point(map_rid,target_vector)
+	nav_mesh_agent_2d.target_position = final_point
 
 
 func _on_vision_body_exited(body: Node2D) -> void:
-    if is_active and body.is_in_group('player'):
-        chase_time.start()
+	if is_active and body.is_in_group('player'):
+		chase_time.start()
 
 
 func _on_vision_body_entered(body: Node2D) -> void:
-     if is_active and body.is_in_group('player'):
-        chase_time.stop()
+	if is_active and body.is_in_group('player'):
+		chase_time.stop()
 
 
 func _on_chase_time_timeout() -> void:
-    transitioned.emit(self,walk_state.name)
+	transitioned.emit(self,walk_state.name)
 
 
 func _on_attack_vision_body_entered(body: Node2D) -> void:
-     if is_active and body.is_in_group('player'):
-        chase_time.stop()
-        transitioned.emit(self,attack_state.name)
+	if is_active and body.is_in_group('player'):
+		chase_time.stop()
+		transitioned.emit(self,attack_state.name)
